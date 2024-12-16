f08008d1c016de2fba81116d8798a4f537f31f32
ayoub <ayoubhofr@gmail.com>
2024-12-16 14:33:23 +0100

    proposition 1

--- src/main/java/org/aepsilon/dto/ProposalDto.java
+++ src/main/java/org/aepsilon/dto/ProposalDto.java
@@ -1,14 +1,10 @@
 package org.aepsilon.dto;
 
-import org.aepsilon.orm.Category;
 import org.aepsilon.orm.Proposal;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProposalDto {
     public Long id;
-    public List<TranslationDto> translations;
 
     public QuestionDto question;
 
@@ -16,7 +12,6 @@ public class ProposalDto {
 
     public ProposalDto(Proposal p, String lg){
         id = p.id;
-        translations =  new ArrayList<>();
-        translations.add(new TranslationDto(p,lg));
+
     }
 }
--- src/main/java/org/aepsilon/service/QuizzService.java
+++ src/main/java/org/aepsilon/service/QuizzService.java
@@ -4,14 +4,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.aepsilon.dto.ProposalDto;
 import org.aepsilon.dto.QuestionDto;
-import org.aepsilon.dto.TranslationDto;
 import org.aepsilon.orm.Proposal;
 import org.aepsilon.orm.Question;
-import org.aepsilon.web.client.TranslateClient;
-import org.aepsilon.web.client.TranslateRequest;
-import org.aepsilon.web.client.TranslateResponse;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,27 +13,25 @@ import java.util.List;
 @ApplicationScoped
 public class QuizzService {
 
-    @Inject
-    TranslateService translateService;
-    public List<QuestionDto> listAllQuestions(){
+    public List<Question> listAllQuestions(){
         List<Question> questions =  Question.listAll();
-        return translateService.translateQuestions(questions);
+        return questions;
     }
 
-    public QuestionDto loadQuestionById(Long questionId){
+    public Question loadQuestionById(Long questionId){
         Question q = Question.findById(questionId);
-        return translateService.translateOneQuestion(q);
+        return q;
     }
 
-    public List<ProposalDto> listProposals(Long questionId){
-        List<Proposal> proposals =  Proposal.listAll();
+    public List<Proposal> listProposals(Long questionId){
+        List<Proposal> proposals = Proposal.find("question.id", questionId).list();
         List<Proposal> result = new ArrayList<>();
         for(Proposal currentProposal:proposals){
             if(currentProposal.id.equals(questionId)){
                 result.add(currentProposal);
             }
         }
-        return translateService.translateProposals(result);
+        return result;
     }
 
 
